37015668f2a5b64e3c7a15eb868e2b5abdffcf69
Ken <109094738+Ken0803@users.noreply.github.com>
2023-04-16 22:03:49 +0300

    Time: 325 ms (5.88%), Space: 64.5 MB (5.88%) - LeetHub

---
+++ 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @param {number} p
+ * @return {number}
+ */
+var minimizeMax = function (nums, p) {
+    nums.sort((a, b) => a - b);
+    if (p == 0) return 0;
+    let distances = [];
+    for (let i = 1; i < nums.length; i++) {
+        distances.push(nums[i] - nums[i - 1]);
+    }
+    distances.sort((a, b) => a - b);
+    let st = 0;
+    let en = distances.length;
+    while (en > st) {
+        let mid = Math.floor((st + en) / 2);
+        let count = 0;
+        console.log(mid);
+        for (let i = 1; i < nums.length; i++) {
+            if (nums[i] - nums[i - 1] > distances[mid])
+                continue;
+            count++;
+            i++;
+        }
+        console.log(count);
+        if (count >= p)
+            en = mid;
+        else
+            st = mid + 1;
+    }
+    console.log(distances, en);
+    return distances[en];
+};
