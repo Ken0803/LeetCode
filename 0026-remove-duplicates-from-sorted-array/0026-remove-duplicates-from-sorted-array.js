0de3eac3fdf2a406f24001c60af809ab73ac8a7d
Ken <109094738+Ken0803@users.noreply.github.com>
2023-04-20 11:07:50 +0300

    Time: 68 ms (81.14%), Space: 44.9 MB (32.82%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    if (nums.length === 0) {
+        return 0;
+    }
+    
+    let j = 0;
+    
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[j]) {
+            j++;
+            nums[j] = nums[i];
+        }
+    }
+    
+    return j + 1;
+};
