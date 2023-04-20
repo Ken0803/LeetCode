0d97ea02e54300235b0b8258f167e003b1cdade1
Ken <109094738+Ken0803@users.noreply.github.com>
2023-04-20 11:17:36 +0300

    Time: 58 ms (67.82%), Space: 42.1 MB (51.67%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let j = 0;
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+    
+    return j;
+};
