98db0b7e2e3e9841bff0fd343e6dbc1b252dbca6
Ken <109094738+Ken0803@users.noreply.github.com>
2023-04-28 01:58:53 +0900

    Time: 828 ms (54.10%), Space: 197.4 MB (6.56%) - LeetHub

--- 2617-minimum-number-of-visited-cells-in-a-grid/2617-minimum-number-of-visited-cells-in-a-grid.js
+++ 2617-minimum-number-of-visited-cells-in-a-grid/2617-minimum-number-of-visited-cells-in-a-grid.js
@@ -2,35 +2,66 @@
  * @param {number[][]} grid
  * @return {number}
  */
-
 var minimumVisitedCells = function(grid) {
-    let m = grid.length, n = grid[0].length;
-    let dp = Array(m).fill(0).map(() => Array(n).fill(Infinity)), colStacks = Array(n).fill(0).map(() => []);
-    dp[m - 1][n - 1] = 1;
-    colStacks[n - 1].push(m - 1);
-    for (let i = m - 1; i >= 0; i--) {
-        let rowStack = i === m - 1 ? [n - 1] : [];
-        for (let j = n - 1; j >= 0; j--) {
-            let colIndex = findIndex(rowStack, grid[i][j] + j);
-            if (colIndex >= 0) dp[i][j] = Math.min(dp[i][j], 1 + dp[i][rowStack[colIndex]]);
-            let colStack = colStacks[j], rowIndex = findIndex(colStack, grid[i][j] + i);
-            if (rowIndex >= 0) dp[i][j] = Math.min(dp[i][j], 1 + dp[colStack[rowIndex]][j]);
-            while (rowStack.length && dp[i][rowStack[rowStack.length - 1]] >= dp[i][j]) rowStack.pop();
-            rowStack.push(j);
-            while (colStack.length && dp[colStack[colStack.length - 1]][j] >= dp[i][j]) colStack.pop();
-            colStack.push(i);
+    let m = grid.length;
+    let n = grid[0].length;
+    let vis = [];
+    let next = [];
+    for(let i = 0 ; i < m ; i ++){
+        vis.push([]);
+        next.push([]);
+        for(let j = 0 ; j < n ; j ++){
+            vis[i].push(0);
+            next[i].push([]);
+            next[i][j].push(i);
+            next[i][j].push(j);
         }
     }
-    return dp[0][0] === Infinity ? -1 : dp[0][0];
-};
-
-function findIndex(stack, maxIndex) {
-    if (!stack.length) return -1;
-    let low = 0, high = stack.length - 1;
-    while (low < high) {
-        let mid = Math.floor((low + high) / 2);
-        if (stack[mid] <= maxIndex) high = mid;
-        else low = mid + 1;
+    function find_next(x,y,axis){
+        if(axis == 0){
+            if(x >= m || next[x][y][axis] == x) return x;
+            return next[x][y][axis] = find_next(next[x][y][axis],y,axis);
+        }
+        else{
+            if(y >= n || next[x][y][axis] == y) return y;
+            return next[x][y][axis] = find_next(x,next[x][y][axis],axis);
+        }
     }
-    return stack[low] <= maxIndex ? low : -1;
+    let qu = [];
+    let qs = 0;
+    qu.push(0);
+    qu.push(0);
+    qu.push(1);
+    vis[0][0] = 1;
+    while(qs < qu.length){
+        let x = qu[qs++];
+        let y = qu[qs++];
+        let distance = qu[qs++];
+        if(x == m-1 && y == n-1)
+            return distance;
+        let d = grid[x][y];
+        for(let wx = x+1 ; wx <= x + d && wx < m ; wx++ ){
+            let tx = wx;
+            wx = find_next(wx,y,0);
+            if(wx > x + d || wx >= m) break;
+            next[tx][y][0] = x+d+1;
+            if(vis[wx][y] == 1) continue;
+            vis[wx][y] = 1;
+            qu.push(wx);
+            qu.push(y);
+            qu.push(distance+1);
+        }
+        for(let wy = y+1 ; wy <= y+d && wy < n ; wy ++){
+            let ty = wy;
+            wy = find_next(x,wy,1);
+            if(wy > y + d || wy >= n) break;
+            next[x][ty][1] = y+d+1;
+            if(vis[x][wy] == 1) continue;
+            vis[x][wy] = 1;
+            qu.push(x);
+            qu.push(wy);
+            qu.push(distance+1);
+        }
+    }
+    return -1;
 };
