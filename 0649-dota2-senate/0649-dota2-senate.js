e29a1d1babb039c5015c147a34a0e593f51864ba
Ken <109094738+Ken0803@users.noreply.github.com>
2023-05-04 16:26:20 +0900

    Time: 72 ms (75.47%), Space: 46.4 MB (67.92%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.js
+++ 0649-dota2-senate/0649-dota2-senate.js
@@ -4,28 +4,29 @@
  */
 var predictPartyVictory = function(senate) {
     let vis = [];
-    let st = 0;
+    let p = 0, en = 0;
     let n = senate.length;
     while(1){
-        st %= n;
-        if(vis[st]){
-            st ++ ;
+        p %= n;
+        if(vis[p]){
+            p ++ ;
             continue ;
         }
-        let en = st+1;
+        let st = en ;
+        en ++;
         while(1){
             en %= n;
             if(en === st){
-                if(senate[st] === 'R') return "Radiant";
+                if(senate[p] === 'R') return "Radiant";
                 else return "Dire";
             }
-            if(vis[en] || senate[st] === senate[en]){
+            if(vis[en] || senate[p] === senate[en]){
                 en ++;
                 continue ;
             }
             vis[en] = 1;
             break ;
         }
-        st ++ ;
+        p ++ ;
     }
 };
