b6196831196e27a07f855bac4daa6efb7797998d
Ken <109094738+Ken0803@users.noreply.github.com>
2023-05-16 14:21:57 +0900

    Time: 59 ms (55.22%), Space: 41.4 MB (98.48%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.js
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.js
@@ -10,15 +10,13 @@
  * @return {ListNode}
  */
 var swapPairs = function(head) {
-    if (!head || !head.next) {
-        return head;
-    }
-    
-    let firstNode = head;
-    let secondNode = head.next;
-    
-    firstNode.next = swapPairs(secondNode.next);
-    secondNode.next = firstNode;
-    
-    return secondNode;
+  function DFS(v){
+      if(!v) return v;
+      let w = v.next;
+      if(!w) return v;
+      v.next = DFS(w.next);
+      w.next = v;
+      return w;
+  }
+  return DFS(head);
 };
